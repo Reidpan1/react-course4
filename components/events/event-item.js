e38d2d77cf0791712ece50894afde16931048534
Reidpan1 <129805612+Reidpan1@users.noreply.github.com>
2023-05-18 18:14:28 -0500

    added css to list and item

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,8 +1,39 @@
+import Link from "next/link";
+
+import Button from '../ui/button';
+import classes from './event-item.module.css';
+
 function EventItem(props) {
-  return <li>
-    <img src="" alt="" />
-    <div></div>
-  </li>;
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>Explore Event</Button>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
