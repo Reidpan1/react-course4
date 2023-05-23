ef682df0fe21381885097a67d3763267f82f5ee3
Reidpan1 <129805612+Reidpan1@users.noreply.github.com>
2023-05-23 15:04:20 -0500

    near end

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,11 +3,17 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 function Button(props) {
-  return (
-    <Link href={props.link} className={classes.btn}>
-      {props.children}
-    </Link>
-  );
+  
+  if(props.link){
+    return (
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
+  }
+return <button className={classes.button} onClick={props.onClick}>{props.children}</button>
+
+
 }
 
 export default Button;
